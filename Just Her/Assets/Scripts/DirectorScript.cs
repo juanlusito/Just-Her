88a754c221aaf843154ef00bf4c7153d851bec6f
delcastillojuan <77298190+delcastillojuan@users.noreply.github.com>
2021-01-25 17:47:11 +0100

    Versión 0.1 - Objetos interactivos
    Implementación de Naninovel en Unity y creación de objetos los cuales, al clicar sobre ellos, provocan cambios en la interfaz de diálogo
    
    Al pulsar en New Game aparecerá una chica llamada Liliana junto a una esfera y un cubo. Al pusalr en uno de los dos objetos, Liliana reacciona de distintas maneras según el objeto pulsado

---
+++ Just Her/Assets/Scripts/DirectorScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Naninovel;
+public class DirectorScript : MonoBehaviour
+{
+    void Update()
+    {
+        IScriptPlayer scriptPlayer = Engine.GetService<IScriptPlayer>();
+        Script playedScript = scriptPlayer.PlayedScript;
+    }
+}
---
+++ Just Her/Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Naninovel;
+public class ObjectInteraction : MonoBehaviour
+{
+    Renderer renderer;
+    public string label;
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    void OnMouseDown()
+    {
+        IScriptPlayer scriptPlayer = Engine.GetService<IScriptPlayer>();
+        Script playedScript = scriptPlayer.PlayedScript;
+        scriptPlayer.PreloadAndPlayAsync (playedScript.Name, label: label);
+    }
+    void OnMouseEnter()
+    {
+        renderer.material.color = new Color (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 0.5f);
+    }
+    void OnMouseExit()
+    {
+        renderer.material.color = new Color (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 1f);
+    }
+}

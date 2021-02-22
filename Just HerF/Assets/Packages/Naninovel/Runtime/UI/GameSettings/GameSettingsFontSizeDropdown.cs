62ff6f5feab4c39dbd988245256247baf990eb32
delcastillojuan <77298190+delcastillojuan@users.noreply.github.com>
2021-02-22 15:53:29 +0100

    Versión 0.4 - Prototipo
    Versión final de la alfa, que contiene la idea final de cómo son los niveles del juego. Ésta es la versión que hemos entregado el 19/02
    
    CAMBIOS:
    - Se ha decidido quitar el movimiento de cámara por no aportar nada a la experiencia de juego
    
    BUGS ENCONTRADOS:
    - Al pinchar en un objeto no se desactiva el código de interacción, por lo que el jugador puede seguir pinchando durante la conversación
    - La lupa que aparece al poner el ratón sobre un objeto interactivo no desaparece durante la transición entre escenas

---
+++ Just HerF/Assets/Packages/Naninovel/Runtime/UI/GameSettings/GameSettingsFontSizeDropdown.cs
@@ -0,0 +1,72 @@
+﻿// Copyright 2017-2020 Elringus (Artyom Sovetnikov). All Rights Reserved.
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Naninovel.UI
+{
+    public class GameSettingsFontSizeDropdown : ScriptableDropdown
+    {
+        [ManagedText("DefaultUI")]
+        protected static string Small = "Pequeño";
+        [ManagedText("DefaultUI")]
+        protected static string Default = "Predeterminado";
+        [ManagedText("DefaultUI")]
+        protected static string Large = "Grande";
+        [ManagedText("DefaultUI")]
+        protected static string ExtraLarge = "Muy grande";
+
+        [Tooltip("Index of the dropdown list associated with default font size.")]
+        [SerializeField] private int defaultSizeIndex = 1;
+
+        private IUIManager uiManager;
+
+        protected override void Awake ()
+        {
+            base.Awake();
+
+            uiManager = Engine.GetService<IUIManager>();
+        }
+        
+        protected override void OnEnable ()
+        {
+            base.OnEnable();
+
+            InitializeOptions();
+            
+            if (Engine.TryGetService<ILocalizationManager>(out var localeManager))
+                localeManager.OnLocaleChanged += HandleLocaleChanged;
+        }
+
+        protected override void OnDisable ()
+        {
+            base.OnDisable();
+            
+            if (Engine.TryGetService<ILocalizationManager>(out var localeManager))
+                localeManager.OnLocaleChanged -= HandleLocaleChanged;
+        }
+
+        protected override void OnValueChanged (int index)
+        {
+            base.OnValueChanged(index);
+
+            uiManager.FontSize = index == defaultSizeIndex ? -1 : index;
+        }
+
+        private void InitializeOptions ()
+        {
+            var options = new List<string> { Small, Default, Large, ExtraLarge };
+            UIComponent.ClearOptions();
+            UIComponent.AddOptions(options);
+
+            var index = uiManager.FontSize == -1 ? defaultSizeIndex : uiManager.FontSize;
+            if (!UIComponent.options.IsIndexValid(index))
+                throw new Exception($"Failed to initialize font size dropdown: current index `{index}` is not available in `Font Sizes` list.");
+            UIComponent.value = index;
+            UIComponent.RefreshShownValue();
+        }
+        
+        private void HandleLocaleChanged (string locale) => InitializeOptions();
+    }
+}
